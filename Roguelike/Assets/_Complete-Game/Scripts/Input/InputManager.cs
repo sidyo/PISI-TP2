01af87c2353f0abf760b17efaf23e2f8b6948e11
VINICIUS TONI BRESSAN <14104178@pucrs.br>
2019-11-13 10:01:31 -0200

    Add base game

---
+++ Roguelike/Assets/_Complete-Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Completed;
+
+public class InputManager : MonoBehaviour
+{
+    public Player player;
+    
+    public void MovePlayer(float hAxis, float vAxis)
+    {
+        if (player != null)
+            player.Move(hAxis, vAxis);
+    }
+}
---
+++ Roguelike/Assets/_Complete-Game/Scripts/Input/InputSource.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputSource : MonoBehaviour
+{
+    public InputManager inputManager;
+    
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        inputManager.MovePlayer(h, v);
+    }
+}
